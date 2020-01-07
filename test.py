8f016cb2625bff828a9f29d7ff4c3777567d68ac
thebonface <bonfaceo3@gmail.com>
2020-01-07 17:54:34 +0300

    initial commit for the blog project

---
+++ test.py
@@ -0,0 +1,31 @@
+#if 5 > 2:
+ #   print('5 is indeed greater than 2')
+#else:
+ #   print('5 is not greater than 2')
+
+#name = 'Sonja'
+
+#if name == 'ola':
+
+    #print('Hey Ola!')
+
+#elif name=='Sonja':
+    #print('Hey Sonja!')
+#else:
+ #   print('Hey annonymous!')
+
+#width = 2,height= 3, length 
+
+
+def area(length,width):
+    total = length * width
+    print total
+
+    return total;
+
+#now call the area function
+
+total = area(10,20)
+    
+
+
